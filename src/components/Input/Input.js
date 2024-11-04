8c7b9b42d7e42cc87f73c7a98eada5f266ebbc55
hyunjiikim <hjkim96727@gmail.com>
2024-11-05 00:01:10 +0100

    Implement Input Components on SignIn Page

--- src/components/Input/Input.js
+++ src/components/Input/Input.js
@@ -1,7 +1,18 @@
-const InputText = (inputType,icon) => {
-    const iconPath = {icon} 
+const InputText = (inputType,icon,PlaceHolder) => {
+    const iconPath = `../../icons/${icon}.png`
 
     return(
-        <input type={inputType} />
+        <input type={inputType}
+        placeholder={`${iconPath} ${PlaceHolder}`}
+        style={{
+            width: '323px',
+            height: '60px',
+            borderRadius: '12px',
+            backgroundColor: 'rgba(240,240,240,0.8)',
+            Color: '#0B0A0A',
+            fontSize:'16pt'
+        }}/>
     );
 };
+
+export default InputText;
--- src/pages/SignIn/SignIn.js
+++ src/pages/SignIn/SignIn.js
@@ -2,3 +2,24 @@ import React from "react";
 import 'SignIn.css';
 
 import Header from '../../components/Header/Header'
+import InputText from "../../components/Input/Input";
+
+const SignInPage = () =>{
+    return(
+        <React.Fragment>
+            < Header/>
+            <InputText
+                inputType='email'
+                icon='Mail'
+                Placeholder='E-mail'
+            />
+             <InputText
+                inputType='password'
+                icon='Lock'
+                Placeholder='Password'
+            />
+        </React.Fragment>
+    );
+};
+
+export default SignInPage;
