78a9a4063e0ffdf3ce3ba2eeb76745b7d20c95cc
Hyunji Kim <88721125+HyunjiiKim@users.noreply.github.com>
2024-11-05 11:50:41 +0000

    Add Button Hover on Button.css

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,13 +1,11 @@
 import React from "react";
 import { cva } from "class-variance-authority";
-import { useState } from "react";
 import './Button.css';
 
 const ButtonStyles=(cva,'base-button'{
     variants: {
         intent:{
             primary: 'buttonPrimary',
-            hover: 'buttonHover',
             inactive: 'buttonInactive'
             danger: 'buttonDanger'
         },
@@ -25,11 +23,6 @@ const ButtonStyles=(cva,'base-button'{
 );
 
 const Button = ({intent, size, input}) => {
-
-    //Add a Event when Mouse is Over on BTN
-    const Hover (e) =>{
-        event.target.style.background=''
-    }
 
     return(
         <button className={ButtonStyles({intent,size})}>
