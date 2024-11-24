47a82a90cafc2670f7ef38082a92e153b603e6a9
hyunjiikim <hjkim96727@gmail.com>
2024-11-24 15:23:57 +0100

    Implement Status

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -6,17 +6,21 @@ import TodoItem from "../../components/TodoItem/TodoItem";
 import { InputDate } from "../../components/Input/Input";
 import Searchbar from "../../components/Searchbar";
 import TodoEditor from '../../components/TodoEditor/TodoEditor';
+import Status from "../../components/Status/Status";
 
 const Home = () => {
 
     return(
         <React.Fragment>
             <Header />
+            <Status />
             <Searchbar />
             <div>
                 <h2>Task for Today</h2>
                 <TodoEditor />
             </div>
+            <TodoItem />
+            <TodoItem />
             <TodoItem />
         </React.Fragment>
     )
