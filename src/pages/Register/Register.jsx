1e4eefd1a527770b5a15b72bd961dee6fd9649b2
hyunjiikim <hjkim96727@gmail.com>
2024-11-24 15:23:35 +0100

    Create Register form

--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -1,12 +1,14 @@
 import React from "react";
+import Form from "../../components/Form/Registerfom";
 
 const Register = () =>{
     return(
         <React.Fragment>
-        <h1>Register</h1>
+            <h2>Personal Information</h2>
+        <Form />
         </React.Fragment>
     )
   
-};
+}
 
 export default Register;
