ed1564bf7a20f4ea658530f54c0c716c9eab38fd
hyunjiikim <hjkim96727@gmail.com>
2024-12-01 12:31:26 +0100

    Update OnChange on NewItem

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,7 +2,7 @@ import React, { useReducer, useRef } from "react";
 
 import Button from "../../components/Button/Button";
 import Header from "../../components/Header/Header";
-import TodoItem from "../../components/TodoItem/TodoItem";
+import TodoList from "../../components/TodoItem/TodoList";
 import { InputDate } from "../../components/Input/Input";
 import Searchbar from "../../components/Searchbar";
 import TodoEditor from '../../components/TodoEditor/TodoEditor';
@@ -38,9 +38,7 @@ const Home = () => {
                 <h2>Task for Today</h2>
                 <TodoEditor />
             </div>
-            <TodoItem />
-            <TodoItem />
-            <TodoItem />
+            <TodoList />
         </React.Fragment>
     )
     
